dc0a05c98f90fcc2e76e469a6c6dfc945c1f53a7
Julien Chaffraix <julien.chaffraix@gmail.com>
2019-11-24 17:19:13 -0800

    Adding a minimal Go container

---
+++ go/src/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "net/http"
+)
+
+func main() {
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello world")
+  })
+  log.Println("Started server")
+  log.Fatal(http.ListenAndServe(":8000", nil))
+}
